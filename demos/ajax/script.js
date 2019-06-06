77c09cd622b74fb2303e55e9cf3ee445dc550a53
Rakesh <rashrestha@mum.edu>
2019-06-06 14:03:00 -0500

    Update script.js

--- demos/ajax/script.js
+++ demos/ajax/script.js
@@ -3,10 +3,10 @@
 
     window.addEventListener("load",function () {
         document.querySelector('#hw').addEventListener("change",function () {
-            var file = 'homeworks/' + document.querySelector('#hw').val();
+            var file = 'homeworks/' + document.querySelector('#hw').value;
             fetch(file)
             .then((data)=>{
-                document.querySelector('#output').val(data);
+                document.querySelector('#output').value = data;
             })
         });
     });
