6f27f39f145f12da5f088fe24a7f10f1581fe75a
Rakesh <rashrestha@mum.edu>
2019-06-06 14:07:18 -0500

    Update script.js

--- demos/ajax/script.js
+++ demos/ajax/script.js
@@ -5,7 +5,7 @@
         document.querySelector('#hw').addEventListener("change",function () {
             var file = 'homeworks/' + document.querySelector('#hw').value;
             fetch(file)
-            .then((res)=>res.json())
+            .then((res)=>{var x = res; console.log(res);})
             .then(data=>{
                 document.querySelector('#output').value = data;
             })
