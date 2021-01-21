5b72f8466cbadcb544c708d8695b63231516db98
Rakesh Shrestha <rashrestha@miu.edu>
2021-01-20 23:14:50 -0600

    Ajax demo updated

--- wap/demos/ajax/script.js
+++ wap/demos/ajax/script.js
@@ -3,7 +3,7 @@
 
     window.addEventListener("load",function () {
         document.querySelector('#hw').addEventListener("change",function () {
-            var file = 'homeworks/' + document.querySelector('#hw').value;
+            let file = 'homeworks/' + document.querySelector('#hw').value;
             fetch(file)
             .then(res=>res.text())
             .then(data=>{
